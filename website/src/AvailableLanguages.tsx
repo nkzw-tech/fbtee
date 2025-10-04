17ee271315b689d2a9410995fa32cdc66d47b150
cpojer <christoph.pojer@gmail.com>
2025-10-04 09:29:06 +0900

    Enable French on the website.

--- website/src/AvailableLanguages.tsx
+++ website/src/AvailableLanguages.tsx
@@ -2,6 +2,7 @@ export default new Map([
   ['en_US', 'English'],
   ['ja_JP', '日本語 (Japanese)'],
   ['de_DE', 'Deutsch (German)'],
+  ['fr_FR', 'Français (French)'],
   ['es_ES', 'Español (Spanish)'],
   ['ru_RU', 'Русский (Russian)'],
   ['fb_HX', 'Pirate (Hack)'],
