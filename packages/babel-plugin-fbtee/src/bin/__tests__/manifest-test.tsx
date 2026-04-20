daf813dd4b29baccbc8b777aa820fab09156b633
Bartosz Żądłowski <bartosz.zadlowski@surferseo.com>
2026-04-20 13:17:21 +0200

    fix: generate manifest files - skip directories with extension in name

--- packages/babel-plugin-fbtee/src/bin/__tests__/manifest-test.tsx
+++ packages/babel-plugin-fbtee/src/bin/__tests__/manifest-test.tsx
@@ -13,4 +13,12 @@ describe('manifest', () => {
     expect(JSON.stringify(files, null, 2)).toMatchSnapshot();
     expect(JSON.stringify(enumManifest, null, 2)).toMatchSnapshot();
   });
+
+  it('should skip directories whose names match the file extension glob', async () => {
+    const srcPath = join(import.meta.dirname, '../__fixtures__');
+
+    const { files } = await generateManifest([srcPath], srcPath);
+
+    expect(files).not.toContain('dir-with-js-extension.js');
+  });
 });
